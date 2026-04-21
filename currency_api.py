9ae5310f2ff691eb250c464de9eb652f9dfc97c9
Alex-Stas <alexander.stashchuk@gmail.com>
2026-04-21 16:23:06 +0300

    api 2 done

--- currency_api.py
+++ currency_api.py
@@ -15,5 +15,18 @@ def get_currency_rate(default: str='USD', currencies: list[str]=['RUB', 'EUR', '
     data = response.json()
     return data
 
+def convert_currency(amount: float, from_currency: str, to_currency: str):
+    url = "https://api.exchangerate.host/convert"
+    params = {
+        'access_key': os.getenv('API_KEY'),
+        'from': from_currency,
+        'to': to_currency,
+        'amount': amount
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    return data
+
 if __name__ == '__main__':
-    print(get_currency_rate())
+    data = get_currency_rate(default="RUB", currencies=["USD", "EUR", "GBP", "JPY", "CNY"])
+    print(data["quotes"])
